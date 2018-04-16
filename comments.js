746af0f25ee059b15637eef42db19a1052e563ae
Marcin <36379394+MarcinBB@users.noreply.github.com>
2018-04-16 12:11:52 +0200

    Update comments.js
    small correct

--- comments.js
+++ comments.js
@@ -1,69 +1,56 @@
-import {ADD_COMMENT, REMOVE_COMMENT, EDIT_COMMENT, THUMB_UP_COMMENT, THUMB_DOWN_COMMENT } from './actions';
+import * as actions from "./actions";
 
-const initialState = {
-  comments: [],
-  users: []
-};
-
-export const comments = (state = [], action) => {
-  switch (action.type) {
-    case ADD_COMMENT:
-    return [{
+function comments(state = [], action) {
+  switch(action.type) {
+    case actions.CREATE_COMMENT:
+    return  [{
       id: action.id,
       text: action.text,
-      votesUp: 0,
-      votesDown: 0
-    }, ...state.comments];
-    break;
+      votes: 0
+    },
+    ...state];
     
-    case EDIT_COMMENT:
-    const check = state.comments.find(comment => (comment.id === action.id));
-    if (check) {
-      const index = state.comments.indexOf(check);
-      return [
-        ...state.comments.slice(0, index),
-        {...state.comments[index], text: action.text},
-        ...state.comments.slice(index+1, state.comments.length)
-      ];
-    } else {
-      return state;
-    }
-    break;
+    case actions.DELETE_COMMENT:
+    return [
+      state.filter( comment => comment.id !== action.id)
+    ];
     
-    case THUMB_UP_COMMENT:
-    const check = state.comments.find(comment => (comment.id === action.id));
-    if (check) {
-      const index = state.comments.indexOf(check);
-      return [
-        ...state.comments.slice(0, index),
-        {...state.comments[index], votesUp: state.comments[index].votesUp + 1},
-        ...state.comments.slice(index+1, state.comments.length)
-      ];
-    } else {
-      return state;
-    }
-    break;
+    case actions.EDIT_COMMENT:
+    return [
+      state.map( comment => {
+        return comment.id === action.id ?
+        Object.assign({}, comment, {
+          text: action.text
+        }) :
+        comment
+      })
+    ]
     
-    case THUMB_DOWN_COMMENT:
-    const check = state.comments.find(comment => (comment.id === action.id));
-    if (check) {
-      const index = state.comments.indexOf(check);
-      return [
-        ...state.comments.slice(0, index),
-        {...state.comments[index], votesDown: state.comments[index].votesDown + 1},
-        ...state.comments.slice(index+1, state.comments.length)
-      ];
-    } else {
-      return state;
-    }
-    break;
+    case actions.THUMB_UP_COMMENT:
+    return [
+      state.map( comment => {
+        return comment.id === action.id ?
+        Object.assign({}, comment, {
+          votes: comment.votes + 1
+        }) :
+        comment
+      })
+    ]    
     
-    case REMOVE_COMMENT:
-    return state.comments.filter(comment => comment.id !== action.id);
-    break;
+    case actions.THUMB_DOWN_COMMENT:
+    return [
+      state.map ( comment => {
+        return comment.id === action.id ?
+        Object.assign({}, comment, {
+          votes: comment.votes - 1
+        }) :
+        comment 
+      })
+    ]    
     
     default:
     return state;
   }
-  
-}
+}        
+
+export default comments;
