8fc5f9e907b3540d2f51b520e98632a4b509a22c
Marcin <36379394+MarcinBB@users.noreply.github.com>
2018-04-16 12:12:39 +0200

    Update action.js
    correction

--- action.js
+++ action.js
@@ -1,62 +1,44 @@
 import uuid from uuid;
 
-const ADD_COMMENT = 'ADD_COMMENT';
-const REMOVE_COMMENT = 'REMOVE_COMMENT';
-const EDIT_COMMENT = 'EDIT_COMMENT';
-const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
-const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+export const ADD_COMMENT = 'ADD_COMMENT',
+ REMOVE_COMMENT = 'REMOVE_COMMENT',
+ EDIT_COMMENT = 'EDIT_COMMENT',
+ THUMB_UP_COMMENT = 'THUMB_UP_COMMENT',
+ THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
 
-// add comment
-const addComment = text => {
-	return {
-		type: ADD_COMMENT,
-		text,
-		id: uuid.v4()
-	}
-};
-
-const boundAddComment = text => dispatch(addComment(text));
-
-// edit comment
-const editComment = text => {
-	return {
-		type: EDIT_COMMENT,
-		text,
-		id
-	}
-};
-
-const boundEditComment = (text, id) => dispatch(editComment(text, id));
-
-
-// remove comment
-const removeComment = id => {
-	return {
-		type: REMOVE_COMMENT,
-		id
-	}
-};
-
-const boundRemoveComment = id => dispatch(removeComment(id));
-
-// thumb up comment
-const thumbUpComment = (id, likes) => {
+export function editComment(id, text) {
     return {
-        type: THUMB_UP_COMMENT,
+        type: EDIT_COMMENT,
         id,
-        likes: ++likes
-    }
-};
-
-const boundThumbUpComment = (id, likes) => dispatch(thumbUpComment(id, likes));
-
-// thumb down comment
-const thumbDownComment = (id, dislikes) => {
-    return {
-        type: THUMB_DOWN_COMMENT,
-        id,
-        dislikes: ++dislikes
+        text
     }
 }
 
-const boundThumbDownComment = (id, likes) => dispatch(thumbDownComment(id, dislikes));
+export function createComment(text) {
+    return {
+        type: CREATE_COMMENT,
+        id: uuid.v4(),
+        text
+    }
+}
+
+export function deleteComment(id) {
+    return {
+        type: DELETE_COMMENT,
+        id
+    }
+}
+
+export function thumbUpComment(id) {
+    return {
+        type: THUMB_UP_COMMENT,
+        id,
+    }
+}
+
+export function thumbDownComment(id) {
+    return {
+        type: THUMB_DOWN_COMMENT,
+        id,
+    }
+}
