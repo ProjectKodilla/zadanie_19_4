7782780ea47c89a7cdd596e44f33b9ab2bb52014
Marcin <36379394+MarcinBB@users.noreply.github.com>
2018-04-16 12:13:15 +0200

    Update reducers.js
    update

--- reducers.js
+++ reducers.js
@@ -1,6 +1,43 @@
-import { combineReducers } from 'redux';
-import comments from './comments';
+import uuid from 'uuid';
 
-const app = combineReducers({
-    comments
-});
+export const EDIT_COMMENT = 'EDIT_COMMENT',
+CREATE_COMMENT = 'CREATE_COMMENT',
+DELETE_COMMENT = 'DELETE_COMMENT',
+THUMB_UP_COMMENT = 'THUMB_UP_COMMENT',
+THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+function editComment(id, text) {
+    return {
+        type: EDIT_COMMENT,
+        id,
+        text
+    }
+}
+
+function createComment(text) {
+    return {
+        type: CREATE_COMMENT,
+        id: uuid.v4
+    }
+}
+
+function deleteComment(id) {
+    return {
+        type: DELETE_COMMENT,
+        id
+    }
+}
+
+function thumbUpComment(id) {
+    return {
+        type: THUMB_UP_COMMENT,
+        id,
+    }
+}
+
+function thumbDownComment(id) {
+    return {
+        type: THUMB_DOWN_COMMENT,
+        id,
+    }
+}
